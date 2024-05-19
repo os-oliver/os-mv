7027b37c44c0c35783f5b63ccacf905b1976ec07
Oliver Stankovic <oliver.stankovic@gmail.com>
2024-05-19 10:46:28 +0200

    4

---
+++ DependencyInjection/OsOliverOSMVBundleExtension.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OsOliver\OSMVBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class OsOliverOSMVBundleExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__) . '/Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
